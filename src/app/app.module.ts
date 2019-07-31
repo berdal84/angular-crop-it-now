68f8c259fdb5245680221ff90fe443a87805acad
berdal84 <contact@dalle-cort.fr>
2019-07-31 17:04:27 +0200

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DragDropModule} from '@angular/cdk/drag-drop';
+import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { HomeComponent } from './home/home.component';
+import { CropComponent } from './crop/crop.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    DragDropModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    HomeComponent,
+    CropComponent
+  ],
+  bootstrap: [ AppComponent  ]
+})
+export class AppModule { }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
---
+++ src/app/crop/crop.component.ts
@@ -0,0 +1,19 @@
+import {Component} from '@angular/core';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
+/**
+ * @title Basic Drag&Drop
+ */
+@Component({
+  selector: 'app-crop',
+  templateUrl: './crop.component.html',
+  styleUrls: ['./crop.component.css'],
+})
+export class CropComponent
+{
+  // todo: update a rectangle each time a rectangle corner is moved to visualize cropped erea
+}
+
+/**  Copyright 2019 Google Inc. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */

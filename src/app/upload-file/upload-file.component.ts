56854725eb26fc6ccb03420cb34d96b7364857c8
berdal84 <contact@dalle-cort.fr>
2019-08-01 13:04:39 +0200

    upload-file component modified to be sure there is only a single uploaded file.

--- src/app/upload-file/upload-file.component.ts
+++ src/app/upload-file/upload-file.component.ts
@@ -10,11 +10,21 @@ export class UploadFileComponent  {
   files: any = [];
 
   uploadFile(event) {
-    for (let index = 0; index < event.length; index++) {
-      const element = event[index];
-      this.files.push(element.name)
-    }  
+
+    /* remove existing files (we only wants a single file to edit */
+    if ( this.files.length > 0)
+    {
+      this.files = [];
+    }
+
+    /* add the first dragged file (draggin multiple files is allowed by browsers, we ignore them)*/
+    if( event.length > 0)
+    {
+      const element = event[0];
+      this.files.push(element.name);    
+    }
   }
+
   deleteAttachment(index) {
     this.files.splice(index, 1)
   }
